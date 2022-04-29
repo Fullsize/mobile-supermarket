5df6ccd7c4237b8f03cbefc7f74510663a6ca84a
无涯 <yutaozhou@YutaodeMacBook-Pro.local>
2022-04-29 16:26:09 +0800

    feat: 底部菜单

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,20 +1,67 @@
 /*
  * @Date: 2022-03-21 16:22:27
  * @LastEditors: Fullsize
- * @LastEditTime: 2022-03-21 16:32:04
- * @FilePath: /vite-react/src/components/layout/index.tsx
+ * @LastEditTime: 2022-04-29 16:22:57
+ * @FilePath: /mobile-supermarket/src/components/layout/index.tsx
  * @Author: Fullsize
  */
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Badge, TabBar } from 'antd-mobile';
+import {
+  AppOutline,
+  MessageOutline,
+  MessageFill,
+  UnorderedListOutline,
+  UserOutline,
+  ShopbagOutline,
+} from 'antd-mobile-icons';
 import styles from './index.module.css';
+
 const Layout: React.FC = (props) => {
+  const [state, setState] = useState('home');
+
+  const tabs = [
+    {
+      key: 'home',
+      title: '首页',
+      icon: <AppOutline />,
+    },
+
+    {
+      key: 'message',
+      title: '我的消息',
+      icon: (active: boolean) =>
+        active ? <MessageFill /> : <MessageOutline />,
+    },
+    {
+      key: 'todo',
+      title: '购物袋',
+      icon: <ShopbagOutline />,
+    },
+    {
+      key: 'personalCenter',
+      title: '个人中心',
+      icon: <UserOutline />,
+    },
+  ]
+  useEffect(()=>{
+    console.log(state)
+  },[state])
   return (
     <div className={styles['constainer']}>
       <div className={styles['content']}>
         {props.children}
       </div>
       <div className={styles['footer']}>
-        11313
+        <TabBar onChange={setState}>
+          {tabs.map(item => (
+            <TabBar.Item
+              key={item.key}
+              icon={item.icon}
+              title={item.title}
+            />
+          ))}
+        </TabBar>
       </div>
     </div>
   )
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,14 +1,22 @@
 /*
  * @Date: 2022-02-16 17:19:07
  * @LastEditors: Fullsize
- * @LastEditTime: 2022-03-14 14:57:33
- * @FilePath: /vite-react/src/routes/index.ts
+ * @LastEditTime: 2022-04-29 16:22:39
+ * @FilePath: /mobile-supermarket/src/routes/index.ts
  * @Author: Fullsize
  */
 import loadable from '@loadable/component';
-export default [{
-  path:'/',
-  view:loadable(() => import(`@src/pages/home`)),
-  name:'首页',
-  children:[]
-}]
+export default [
+  {
+    path: '/',
+    view: loadable(() => import(`@src/pages/home`)),
+    name: '首页',
+    children: []
+  },
+  // {
+  //   path: '/message',
+  //   view: loadable(() => import(`@src/pages/message`)),
+  //   name: '首页',
+  //   children: []
+  // }
+]
